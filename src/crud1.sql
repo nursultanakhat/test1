a60c9fa60b37ddd9b5740a6806a8f0aaae3f5dce
user <250812@astanait.edu.kz>
2026-01-16 13:42:17 +0300

    Add sql crud operations

---
+++ src/crud1.sql
@@ -0,0 +1,6 @@
+INSERT INTO vehicle (model, price, available)
+VALUES ('Toyota Camry', 50.0, true);
+
+UPDATE vehicle SET price = 60.0 WHERE id = 10;
+DELETE FROM vehicle WHERE id = 9;
+SELECT * FROM vehicle;
---
+++ src/crud2.sql
@@ -0,0 +1,8 @@
+INSERT INTO client (name, age)
+VALUES ('Nursultan', 21);
+
+UPDATE client SET age = 22 WHERE id = 12;
+
+DELETE FROM client WHERE id = 10;
+
+SELECT * FROM client;
