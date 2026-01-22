45f2260419966059db7fe9779db9f9447163112a
nursultanakhat <250812@astanait.edu.kz>
2026-01-22 20:10:42 +0300

    Delete src/crud1.sql

--- src/crud1.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO vehicle (model, price, available)
-VALUES ('Toyota Camry', 50.0, true);
-
-UPDATE vehicle SET price = 60.0 WHERE id = 1;
-DELETE FROM vehicle WHERE id = 2;
-SELECT * FROM vehicle;
